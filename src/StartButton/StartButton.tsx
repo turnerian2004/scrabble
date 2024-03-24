1f6ae03e14123851786a286436081c4844eea2fc
Ian Turner <turnerian2004@gmail.com>
2024-03-24 17:11:41 +0100

    feature: add animated pulse to start btn

--- src/StartButton/StartButton.tsx
+++ src/StartButton/StartButton.tsx
@@ -12,14 +12,23 @@ interface IStartButton {
 
 export const StartButton: React.FC<IStartButton> = ({ dispatch }) => {
     return (
-        <Button
-            style={{ textTransform: 'none' }}
-            variant="outlined"
-            onClick={() => {
-                dispatch({ type: UserActions.STARTGAME })
-            }}
-        >
-            Start!
-        </Button>
+        <>
+            <Button
+                style={{ textTransform: 'none' }}
+                variant="outlined"
+                onClick={() => {
+                    dispatch({ type: UserActions.STARTGAME })
+                }}
+                className="absolute"
+            >
+                <div className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2">
+                    <span className="relative flex h-3 w-3">
+                        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                        <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+                    </span>
+                </div>
+                Start!
+            </Button>
+        </>
     )
 }
