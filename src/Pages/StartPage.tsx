abe8704a40dfb583ecc2bee6f9daeec0ac40b976
Ian Turner <turnerian2004@gmail.com>
2024-05-19 07:21:59 +0200

    feature: add delay to start page

--- src/Pages/StartPage.tsx
+++ src/Pages/StartPage.tsx
@@ -4,8 +4,13 @@ import { useStartButton } from '../Context/CustomHooks'
 import { useNavigate } from 'react-router-dom'
 import { UserButton } from '../Components/UserButton'
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward'
+import { useEffect, useState } from 'react'
 
 export const StartPage = () => {
+    const [showButton, setShowButton] = useState(false)
+    const [showArrowDownwardIcon, setShowArrowDownwardIcon] =
+        useState(false)
+
     const { startGame } = useStartButton()
     const navigate = useNavigate()
 
@@ -14,18 +19,36 @@ export const StartPage = () => {
         navigate('/visitor')
     }
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowArrowDownwardIcon(true)
+        }, 2000)
+        return () => clearTimeout(timer)
+    }, [])
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowButton(true)
+        }, 2500)
+        return () => clearTimeout(timer)
+    }, [])
+
     return (
         <div className="grid grid-cols-2 gap-4 h-screen justify-center">
             <div></div>
             <div className="flex flex-col justify-center items-center">
                 <IntroCard message={welcomeMessage}></IntroCard>
-                <div className="animate-bounce">
-                    <ArrowDownwardIcon></ArrowDownwardIcon>
-                </div>
-                <UserButton
-                    onClick={handleStartGame}
-                    title={'Start!'}
-                ></UserButton>
+                {showArrowDownwardIcon && (
+                    <div className="animate-bounce">
+                        <ArrowDownwardIcon></ArrowDownwardIcon>
+                    </div>
+                )}
+                {showButton && (
+                    <UserButton
+                        onClick={handleStartGame}
+                        title={'Start!'}
+                    ></UserButton>
+                )}
             </div>
         </div>
     )
